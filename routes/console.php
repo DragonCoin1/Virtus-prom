5e526a0ca53a61400d569c8874dd5908743b2090
DragonCoin1 <teddymyfirxnews@gmail.com>
2026-01-19 19:21:58 +0300

    Implement routes CSV import

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('promoters:clear', function () {
+    $this->info('Removing related route actions...');
+    DB::table('route_actions')->delete();
+
+    $this->info('Removing promoters...');
+    $deleted = DB::table('promoters')->delete();
+
+    $this->info("Done. Deleted promoters: {$deleted}.");
+})->purpose('Delete all promoters and related route actions');
